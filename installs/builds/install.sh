0c6e579e9f861a3c9e31fe9f30cca5eabcb6b069
Markus Übeleis <office@cope-it.at>
2023-04-08 15:31:50 +0200

    dev

---
+++ installs/builds/install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+get_pkg=""
+# Check for curl
+if [ -x "$(command -v curl)" ]; then
+  get_pkg="curl"
+  echo "curl is installed"
+  echo "Example usage: curl https://example.com"
+fi
+
+# Check for wget
+if [ -x "$(command -v wget)" ]; then
+  get_pkg="wget"
+  echo "wget is installed"
+  echo "Example usage: wget https://example.com"
+fi
+
+# Check if both curl and wget are missing
+if [ -z "$get_pkg" ]; then
+  echo "Either curl or wget must be installed"
+  exit;
+fi
+
+# Check if apt-get (Debian package manager) is installed
+if [ -x "$(command -v apt-get)" ]; then
+    echo "Debian-based distribution detected"
+
+    wget https://agent.server-watchdog.com/itmon.deb
+    dpkg -i itmon.deb
+    apt-get -f install
+    dpkg -i itmon.deb
+
+# Check if yum (RPM package manager) is installed
+elif [ -x "$(command -v yum)" ]; then
+    echo "RPM-based distribution detected"
+    # Install the package using yum
+    yum install mypackage
+else
+    echo "Unsupported distribution detected"
+    exit 1
+fi
