c276ab14ead7729f441ffc958d59c5d4a3684db5
Markus Übeleis <office@cope-it.at>
2023-04-10 16:51:34 +0200

    dev

--- installs/debian/build.sh
+++ installs/debian/build.sh
@@ -6,6 +6,8 @@ cp ../../*.py ./itmon/usr/lib/itmon/
 mv ./itmon/usr/lib/itmon/main.py ./itmon/usr/lib/itmon/itmon.reporter.py
 
 cp ../../itmon.config.json ./itmon/usr/lib/itmon/itmon.config.json
+
+chmod 0555 ./itmon/DEBIAN/prerm
 dpkg-deb --build itmon
 if [ ! -d "../builds" ]; then
   mkdir "../builds"
