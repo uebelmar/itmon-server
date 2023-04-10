2b63ea6ed0db55de289de4b5e0ebdcd9686d787e
Markus Übeleis <office@cope-it.at>
2023-04-10 17:41:31 +0200

    dev

--- installs/builds/install.sh
+++ installs/builds/install.sh
@@ -18,36 +18,35 @@ fi
 # Check if both curl and wget are missing
 if [ -z "$get_pkg" ]; then
   echo "Either curl or wget must be installed"
-  exit;
+  exit
 fi
-
 
 # todo: check if token is unused
 
-
 # Check if apt-get (Debian package manager) is installed
 if [ -x "$(command -v apt-get)" ]; then
-    echo "Debian-based distribution detected"
+  echo "Debian-based distribution detected"
 
-    if [ -f "itmon.deb" ]; then
-      rm itmon.deb
-
-      apt-get update
-      apt-get install build-essential python3-dev -y
+  if [ -f "itmon.deb" ]; then
+    rm itmon.deb
   fi
-    $get_pkg https://agents.server-watchdog.com/itmon.deb
-    dpkg -i itmon.deb
-    apt-get -f install -y
-    dpkg -i itmon.deb
+
+  apt-get update
+  apt-get install build-essential python3-dev -y
+
+  $get_pkg https://agents.server-watchdog.com/itmon.deb
+  dpkg -i itmon.deb
+  apt-get -f install -y
+  dpkg -i itmon.deb
 
 # Check if yum (RPM package manager) is installed
 elif [ -x "$(command -v yum)" ]; then
-    echo "RPM-based distribution detected"
-    # Install the package using yum
-    yum install mypackage
+  echo "RPM-based distribution detected"
+  # Install the package using yum
+  yum install mypackage
 else
-    echo "Unsupported distribution detected"
-    exit 1
+  echo "Unsupported distribution detected"
+  exit 1
 fi
 
-echo "{\"token\": \"$1\",\"interval\": 20, \"apiUrl\": \"https://api.server-watchdog.com/v1\"}" > /usr/lib/itmon/itmon.config.json
+echo "{\"token\": \"$1\",\"interval\": 20, \"apiUrl\": \"https://api.server-watchdog.com/v1\"}" >/usr/lib/itmon/itmon.config.json
